1b34b023d49585f96eea5b61ddbe3d3d35105c4a
blackLearning <fuyigekua@foxmail.com>
2017-08-29 17:52:56 +0800

    add examples

---
+++ examples/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { Button } from '../../src/';
+
+class App extends Component {
+  render() {
+    return (
+      <Button />
+    );
+  }
+}
+
+export default App;
