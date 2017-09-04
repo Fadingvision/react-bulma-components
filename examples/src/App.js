49b9ad317f199a4c5dd2ffca982ed4cb7d55e7a5
blackLearning <fuyigekua@foxmail.com>
2017-09-04 18:37:13 +0800

    modify exam

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Button, notification, Progress } from '../../src';
+import { Button, notification, LinearProgress } from '../../src';
 
 class App extends Component {
   componentDidMount() {
     notification({
       msg: 'type',
       type: 'primary',
-      closeTimeoutMS: 0,
+      closeTimeoutMS: 0
     })
   }
 
   render() {
     return <div>
       <Button type="primary">123</Button>
-      <Progress value="80" type="primary"></Progress>
+      <LinearProgress value="50" mode="determinate" type="primary" />
     </div>;
   }
 }
