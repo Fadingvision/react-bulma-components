7ae3381b1b272f91b621505bfa72e4458f1288f5
blackLearning <fuyigekua@foxmail.com>
2017-09-12 13:40:53 +0800

    update examples

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Button, notification, LinearProgress, Breadcrumb } from '../../src';
-
+import {
+  Button,
+  notification,
+  LinearProgress,
+  Breadcrumb,
+  DropDown,
+  Menu
+} from '../../src';
 class App extends Component {
   componentDidMount() {
   }
 
-  openNotification() {
+  openNotification(msg) {
     notification({
-      msg: 'notification',
+      msg,
       type: 'primary',
-      closeTimeoutMS: 0
+      closeTimeoutMS: 2000
     })
   }
 
   render() {
     return <div>
-      <Button type="primary" onClick={this.openNotification}>open Notification</Button>
+      <Button type="primary" onClick={() => this.openNotification('notification')}>open Notification</Button>
       <LinearProgress value="50" mode="indeterminate" type="primary" />
       <Breadcrumb separator="succeeds">
         <Breadcrumb.Item><a>Documentation</a></Breadcrumb.Item>
@@ -25,6 +31,22 @@ class App extends Component {
         <Breadcrumb.Item><a>Components</a></Breadcrumb.Item>
         <Breadcrumb.Item active><a>Breadcrumb</a></Breadcrumb.Item>
       </Breadcrumb>
+
+      <DropDown title="hover me!" triggerAction="click">
+        <Menu>
+          <Menu.Item onClick={() => this.openNotification('enu.Item')}>
+            <a target="_blank" rel="noopener noreferrer">1st menu item</a>
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer">2nd menu item</a>
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer">3d menu item</a>
+          </Menu.Item>
+        </Menu>
+      </DropDown>
     </div>;
   }
 }
