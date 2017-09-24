354249cc3f8906222916e7cfe8efa1e2f80d07b4
blackLearning <fuyigekua@foxmail.com>
2017-09-24 21:11:21 +0800

    Modify examle

--- examples/src/App.js
+++ examples/src/App.js
@@ -8,6 +8,7 @@ import {
   Breadcrumb,
   DropDown,
   Menu,
+  PullMore,
   Modal
 } from '../../src';
 class App extends Component {
@@ -23,37 +24,131 @@ class App extends Component {
     })
   }
 
+  renderBulma() {
+    return (
+      <div>
+        <Button type="primary" onClick={() => this.openNotification('notification')}>open Notification</Button>
+        <LinearProgress value="50" mode="indeterminate" type="primary" />
+        <Breadcrumb separator="succeeds">
+          <Breadcrumb.Item><a>Documentation</a></Breadcrumb.Item>
+          <Breadcrumb.Item><a>Bulma</a></Breadcrumb.Item>
+          <Breadcrumb.Item><a>Components</a></Breadcrumb.Item>
+          <Breadcrumb.Item active><a>Breadcrumb</a></Breadcrumb.Item>
+        </Breadcrumb>
+
+        <DropDown title="hover me!" triggerAction="click">
+          <Menu>
+            <Menu.Item onClick={() => this.openNotification('enu.Item')}>
+              <a target="_blank" rel="noopener noreferrer">1st menu item</a>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item>
+              <a target="_blank" rel="noopener noreferrer">2nd menu item</a>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item>
+              <a target="_blank" rel="noopener noreferrer">3d menu item</a>
+            </Menu.Item>
+          </Menu>
+        </DropDown>
+        <hr/>
+        <Button type="primary" onClick={() => this.setState({open: true})}>open Modal</Button>
+        <Modal open={this.state.open}>
+          <div className="example-modal">123</div>
+        </Modal>
+      </div>
+    )
+  }
+
   render() {
     return <div>
-      <Button type="primary" onClick={() => this.openNotification('notification')}>open Notification</Button>
-      <LinearProgress value="50" mode="indeterminate" type="primary" />
-      <Breadcrumb separator="succeeds">
-        <Breadcrumb.Item><a>Documentation</a></Breadcrumb.Item>
-        <Breadcrumb.Item><a>Bulma</a></Breadcrumb.Item>
-        <Breadcrumb.Item><a>Components</a></Breadcrumb.Item>
-        <Breadcrumb.Item active><a>Breadcrumb</a></Breadcrumb.Item>
-      </Breadcrumb>
-
-      <DropDown title="hover me!" triggerAction="click">
-        <Menu>
-          <Menu.Item onClick={() => this.openNotification('enu.Item')}>
-            <a target="_blank" rel="noopener noreferrer">1st menu item</a>
-          </Menu.Item>
-          <Menu.Divider />
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer">2nd menu item</a>
-          </Menu.Item>
-          <Menu.Divider />
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer">3d menu item</a>
-          </Menu.Item>
-        </Menu>
-      </DropDown>
-      <hr/>
-      <Button type="primary" onClick={() => this.setState({open: true})}>open Modal</Button>
-      <Modal open={this.state.open}>
-        <div className="example-modal">123</div>
-      </Modal>
+      <div>
+        <ul>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+          <li>sd</li>
+        </ul>
+      </div>
+      <PullMore>
+        <ul className="pull-more">
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+        </ul>
+      </PullMore>
     </div>;
   }
 }
