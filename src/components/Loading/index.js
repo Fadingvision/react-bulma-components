23ad3862733e2146c52270e953ba715e69a39550
blackLearning <fuyigekua@foxmail.com>
2017-10-02 22:37:47 +0800

    Add Loading Component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './loading.local.less';
+
+function Loading() {
+  return (
+    <div className="load-effect">
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
+
+export default Loading;
