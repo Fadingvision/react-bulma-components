04357299bfaa99b68f5354a0a67aa0648e0226e4
blackLearning <fuyigekua@foxmail.com>
2017-10-24 09:05:39 +0800

    Add Content for Popover

--- src/components/Popover/index.js
+++ src/components/Popover/index.js
@@ -10,72 +10,13 @@ function PopoverContent(props) {
   };
   return (
     <div>
-      <div className="am-popover-mask" onClick={props.close} />
+      <div className="am-popover-mask" onClick={props.close} onTouchMove={props.close} />
       <div
         className="am-popover fortest am-popover-placement-bottomRight"
         style={style}
       >
         <div className="am-popover-content">
-          <div className="am-popover-inner">
-            <div className="am-popover-item">
-              <div className="am-popover-item-container">
-                <span className="am-popover-item-icon" aria-hidden="true">
-                  <svg>
-                    <use
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      xlinkHref="#popup_enquiry--fill-666"
-                    />
-                  </svg>
-                </span>
-                <span className="am-popover-item-content">采购询价</span>
-              </div>
-            </div>
-            <div className="am-popover-item">
-              <div className="am-popover-item-container">
-                <span className="am-popover-item-icon" aria-hidden="true">
-                  <svg>
-                    <use
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      xlinkHref="#popup_copy--fill-666"
-                    />
-                  </svg>
-                </span><span className="am-popover-item-content">
-                  <span className="js-copy-btn" data-clipboard-text="22">
-                    复制OE号
-                  </span>
-                </span>
-              </div>
-            </div>
-            <div className="am-popover-item am-popover-item-disabled">
-              <div className="am-popover-item-container">
-                <span className="am-popover-item-icon" aria-hidden="true">
-                  <svg>
-                    <use
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      xlinkHref="#popup_fit--fill-666"
-                    />
-                  </svg>
-                </span><span className="am-popover-item-content">
-                  适配车型
-                </span>
-              </div>
-            </div>
-            <div className="am-popover-item">
-              <div className="am-popover-item-container">
-                <span className="am-popover-item-icon" aria-hidden="true">
-                  <svg>
-                    <use
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      xlinkHref="#popup_collect--fill-f1e05a"
-                    />
-                  </svg>
-                </span>
-                <span className="am-popover-item-content">
-                  <span className="collect-active">取消收藏</span>
-                </span>
-              </div>
-            </div>
-          </div>
+          {props.content}
         </div>
       </div>
     </div>
@@ -90,7 +31,8 @@ export default class Popover extends React.Component {
     if (this.props.visible) {
       const component = React.createElement(PopoverContent, {
         close: this.props.close,
-        position
+        position,
+        content: this.props.content,
       });
       ReactDOM.render(component, div);
     }
@@ -103,9 +45,16 @@ export default class Popover extends React.Component {
       const position = this.calcuPosition();
       const component = React.createElement(PopoverContent, {
         close: this.props.close,
-        position
+        position,
+        content: newProps.content,
       });
       ReactDOM.render(component, div);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.visible) {
+      ReactDOM.unmountComponentAtNode(div);
     }
   }
 
